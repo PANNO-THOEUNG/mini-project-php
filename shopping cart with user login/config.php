8e2ca6643b046ff935ff038241a6ab8010bc9175
PANNO THOEUNG <pannothoeung4@gmail.com>
2025-05-08 17:25:39 +0700

    Add files via upload

---
+++ shopping cart with user login/config.php
@@ -0,0 +1,8 @@
+<?php
+     $server = "localhost";
+     $user = "root"; 
+     $password = "1234"; 
+     $db_name = "shop_db";
+     
+     $conn = mysqli_connect($server, $user, $password, $db_name);
+?>
